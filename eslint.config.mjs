e82d8dc68adc87d8963014ac76f83bee877d8ff5
Z Agent <z@container>
2026-08-26 21:02:37 +0000

    feat: add design tokens and UI primitives

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import next from "eslint-config-next";
+import nextTypescript from "eslint-config-next/typescript";
 
 export default defineConfig([
   ...next,
+  ...nextTypescript,
   globalIgnores([
     ".next/**",
     "out/**",
